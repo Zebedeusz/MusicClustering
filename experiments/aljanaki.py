78c313422892670f18863564dfa1d689e0b91c31
michal.gil <michal.gil@globallogic.com>
2018-04-19 15:00:41 +0200

    Improved aljanaki annotations gen.

--- experiments/aljanaki.py
+++ experiments/aljanaki.py
@@ -1,4 +1,5 @@
 annotations_path = "/media/michal/HDD/Music Emotion Datasets/Decoded/data_statistics/aljanaki_simple.csv"
+annotations_path_local = "../annotations/aljanaki_1.csv"
 
 
 def simplify_annotations_file():
@@ -8,16 +9,17 @@ def simplify_annotations_file():
     import numpy
     from experiments.utilities import load_annotations
 
-    entire_anns = load_annotations(anns_filepath)
+    entire_anns = load_annotations(annotations_path_local)
 
     simplified_annotations = []
     for i in range(elements_qnt):
         anns_for_id = []
         for row in entire_anns:
-            if row[0] is not i:
+            if not int(row[0]) == i + 1:
                 break
             anns_for_id.append(row[1:])
-        simplified_annotations.append(numpy.mean(anns_for_id, axis=0))
+        if len(anns_for_id) > 0:
+            simplified_annotations.append(numpy.mean(anns_for_id, axis=0))
     simplified_annotations = numpy.array(simplified_annotations)
 
     numpy.savetxt(annotations_path, simplified_annotations, delimiter=",")
